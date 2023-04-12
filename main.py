6becab111e9715ad04eaf577b7d978d18494404b
Omer Goshen <omergoshen3108@gmail.com>
2023-04-12 19:14:48 +0300

    feat: added generate_random_word function

--- main.py
+++ main.py
@@ -1,25 +1,30 @@
 import gensim.downloader as api
 from random_word import RandomWords
 
-def main():
-    print("Loading the word vectors")
-    word_vectors = api.load("word2vec-google-news-300")
-    print("Done")
-    
+def generate_random_word(model):
     random = RandomWords()  
     while True:
         random_word = random.get_random_word()
-        if random_word in word_vectors.key_to_index.keys():
+        if random_word in model.key_to_index.keys():
             break
-    print("The random word is: ", random_word)
+    return random_word
+
+
+def main():
+    print("Loading the word vectors")
+    model = api.load("word2vec-google-news-300")
+    print("Done")
+    
+    random_word = generate_random_word(model)
+    print(random_word)
     
     done = False
     while not done:
         user_input = input("enter word: ")
-        if user_input not in word_vectors.key_to_index.keys():
+        if user_input not in model.key_to_index.keys():
             print("given word does not exsists")
         
-        similarity = word_vectors.similarity(random_word, user_input)
+        similarity = model.similarity(random_word, user_input)
         print (similarity)
         
         if similarity == 1:
